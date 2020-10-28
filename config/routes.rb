224d0fe2ae23499d5bdc7f77e34f79956c7a35f4
Butler, Alexis (UG - Computer Science) <ab02259@surrey.ac.uk>
2020-10-28 10:43:25 +0000

    error: used slash not hash. Fixed error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'product/index'
+  root 'product#index'
   resources :product, only: [:index, :show] do
     resources :review, only: [:create, :destroy]
   end
@@ -10,11 +10,11 @@ Rails.application.routes.draw do
   end
   resources :order, only: [:create, :show]
 
-  get '/signup', to: 'user#create'
-  post '/signup', to: 'user#create'
-  get '/shutdown', to: 'user#destroy'
-  get '/login', to: 'session#create'
-  get '/logout', to: 'session#destroy'
+  get '/signup' =>'user#create'
+  post '/signup' => 'user#create'
+  get '/shutdown' => 'user#destroy'
+  get '/login' => 'session#create'
+  get '/logout' => 'session#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
