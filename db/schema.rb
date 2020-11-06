5de8686aed1d8afa6cff105561cc553a1f278545
Butler, Alexis (UG - Computer Science) <ab02259@surrey.ac.uk>
2020-11-06 14:07:31 +0000

    Migration in prep to email user once order placed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_140234) do
+ActiveRecord::Schema.define(version: 2020_11_06_140503) do
 
   create_table "order_entries", force: :cascade do |t|
     t.integer "orderID"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_11_06_140234) do
     t.integer "totalCost"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
   create_table "products", force: :cascade do |t|
