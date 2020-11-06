0813ef2f392f5385f69d00278066e67919430f82
Butler, Alexis (UG - Computer Science) <ab02259@surrey.ac.uk>
2020-11-06 13:48:45 +0000

    Added product and section models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+products = Product.create([name: "Test Item", description: "This item is a test", stockCount: 2020, pricePence: 200])
