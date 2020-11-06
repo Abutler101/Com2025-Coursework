147aa1593dd8e823c65981d7eecdbca1647b3a4b
Butler, Alexis (UG - Computer Science) <ab02259@surrey.ac.uk>
2020-11-06 16:16:29 +0000

    Product and section seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,31 +16,69 @@ sect3 = Section.find_or_create_by! name: 'Transport'
 Product.destroy_all
 
 sect1.products.create!({
-
+                           name: "I heart Rails T-shirt",
+                           description: "Unisex I hear rails shirt. So hideous, your preference of web frameworks won't
+                                         be the main reason people avoid you",
+                           stockCount: 20,
+                           pricePence: 772,
+                           image: openAsset("Clothes1.png")
                        })
 sect1.products.create!({
-
+                           name: "Short shorts",
+                           description: "Pair with rails shirt for an iconic unforgettable look",
+                           stockCount: 10,
+                           pricePence: 7072,
+                           image: openAsset("Clothes2.png")
                        })
 sect1.products.create!({
-
+                           name: "Flipflops",
+                           description: "The inferior type of flip flop, pair with socks to complete the look",
+                           stockCount: 19,
+                           pricePence: 1900,
+                           image: openAsset("Clothes3.png")
                        })
 
 sect2.products.create!({
-
+                           name: "Carrot",
+                           description: "Orange thing that came out the ground",
+                           stockCount: 2000,
+                           pricePence: 80,
+                           image: openAsset("Food1.png")
                        })
 sect2.products.create!({
-
+                           name: "Bread",
+                           description: "This doesn't look right",
+                           stockCount: 20,
+                           pricePence: 100,
+                           image: openAsset("Food2.png")
                        })
 sect2.products.create!({
-
+                           name: "Water",
+                           description: "Container not included",
+                           stockCount: 2000,
+                           pricePence: 9726,
+                           image: openAsset("Food3.png")
                        })
 
 sect3.products.create!({
-
+                           name: "Scooter",
+                           description: "skoot skoot",
+                           stockCount: 67,
+                           pricePence: 7082,
+                           image: openAsset("Trans1.png")
                        })
 sect3.products.create!({
-
+                           name: "Car",
+                           description: "Must be over 17 to buy. Must be under 2 to think this is what a car looks like",
+                           stockCount: 1,
+                           pricePence: 6000064,
+                           image: openAsset("Trans2.png")
                        })
 sect3.products.create!({
-
+                           name: "Skateboard",
+                           description: "1: No this is not a good idea. 2: No you are not as good as you think you are.
+                                         3: Yes this is as dangerous as people say it is",
+                           stockCount: 56,
+                           pricePence: 9375,
+                           image: openAsset("Trans3.png")
                        })
