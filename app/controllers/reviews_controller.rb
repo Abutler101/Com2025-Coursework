1c8b43aceb270d28df9218c2de34c8928537799f
Butler, Alexis (UG - Computer Science) <ab02259@surrey.ac.uk>
2020-11-09 13:33:10 +0000

    Integrate reviews into product page. Currently reviews are anonymous, need to make users a thing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,26 @@
 class ReviewsController < ApplicationController
   def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.create(review_params)
+    @review.user = User.first
+
+    if @review.save
+      flash[:success] = "Review Posted"
+    else
+      flash[:danger] = "Failed to post Try again"
+    end
+    redirect_to product_url(@product.id)
   end
 
   def destroy
+    @review = Review.find params[:id]
+    @review.destroy
+    flash[:success] = "Review Deleted"
+    redirect_to product_url(@review.product_id)
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:rating, :content)
   end
 end
