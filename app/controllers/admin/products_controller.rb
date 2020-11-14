a65de2caf029e5568ea714f70f33d210332164dc
Butler, Alexis (UG - Computer Science) <ab02259@surrey.ac.uk>
2020-11-14 14:00:28 +0000

    Products admin system

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,13 +1,31 @@
 class Admin::ProductsController < Admin::AuthorisedController
   def index
+    @products = Product.order(id: :desc).all
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:success] = "New product created"
+      redirect_to admin_products_path
+    else
+      render :'admin/products/new'
+    end
   end
 
   def new
+    @product = Product.new
   end
 
   def destroy
+    @product = Product.find params[:id]
+    @product.destroy
+    flash[:success] = "Product deleted"
+    redirect_to admin_products_path
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :stockCount, :pricePence, :image)
   end
 end
