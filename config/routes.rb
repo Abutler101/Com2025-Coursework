0184cffd43844b99ca4db0425a28f525375a9491
Butler, Alexis (UG - Computer Science) <ab02259@surrey.ac.uk>
2020-11-14 10:26:24 +0000

    Add header and footer basic content. Added legal page to credit relevant gems and identify any 3rd party content used

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+  get 'legal' => 'legal#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
