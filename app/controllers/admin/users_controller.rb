ec804914669a4cd6a91b3c8bf53627ab9f928244
Butler, Alexis (UG - Computer Science) <ab02259@surrey.ac.uk>
2020-11-18 08:46:08 +0000

    User admin dash complete

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,31 @@
 class Admin::UsersController < Admin::AuthorisedController
   def index
+    @users = User.order(id: :desc).all
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "New user created"
+      redirect_to admin_users_path
+    else
+      render :'admin/users/new'
+    end
   end
 
   def new
+    @user = User.new
   end
 
   def destroy
+    @user = User.find params[:id]
+    @user.destroy
+    flash[:success] = "User deleted"
+    redirect_to admin_users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:firstName, :lastName, :email, :password, :password_confirmation, :admin)
   end
 end
