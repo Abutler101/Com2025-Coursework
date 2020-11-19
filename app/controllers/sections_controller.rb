bad6c88e45fbc5e8cab2d545f7fa66e320adc4be
Butler, Alexis (UG - Computer Science) <ab02259@surrey.ac.uk>
2020-11-19 14:37:28 +0000

    Made header navbar conditional and dodged making drop down for sections

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,4 +1,7 @@
 class SectionsController < ApplicationController
+  def index
+    @sections = Section.all
+  end
   def show
     @section = Section.find(params[:id])
     @products = @section.products.order(created_at: :asc)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :products, only: %i[index show] do
     resources :reviews, only: %i[create destroy]
   end
-  resources :sections, only: [:show]
+  resources :sections, only: %i[index show]
   resource :carts, only: [:show] do
     put :add_item
     delete :remove_item
