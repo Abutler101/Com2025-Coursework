c6cdf48b3202c5546e38f174879e97e47da4603c
Butler, Alexis (UG - Computer Science) <ab02259@surrey.ac.uk>
2020-11-21 20:18:30 +0000

    Config money to avoid depreciation warnings

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,5 +1,6 @@
 MoneyRails.configure do |config|
   # set the default currency
   config.default_currency = :gbp
-
+  Money.locale_backend = :currency
+  Money.rounding_mode = BigDecimal::ROUND_HALF_EVEN
 end
