77e4cb915c39aac6d1cc6204b453d8984ea6fe5b
Butler, Alexis (UG - Computer Science) <ab02259@surrey.ac.uk>
2020-11-22 15:24:59 +0000

    Remove Duplicate require tree from JS that was upsetting rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,3 @@
-// This is a manifest file that'll be compiled into including all the files listed below.
-// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
-// be included in the compiled file accessible from http://example.com/assets/application.js
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-//= require jquery
-//= require jquery_ujs
-//= require h5bp
-//= require bootstrap-sprockets
-//= require_tree .
-
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -27,6 +15,8 @@
 //= require jquery.validate
 //= require activestorage
 //= require turbolinks
+//= require h5bp
+//= require bootstrap-sprockets
 //= require_tree .
 
 function  ValidateUserForm() {
