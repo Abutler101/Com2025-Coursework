8b6ef59840d0163bc61c7f2eb9c4e950ab322c38
Butler, Alexis (UG - Computer Science) <ab02259@surrey.ac.uk>
2020-11-23 16:45:14 +0000

    Started Review model tests

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save without product id' do
+
+  end
+  test 'should not save without user id' do
+
+  end
+  test 'should not save without content' do
+
+  end
+  test 'should not save without rating' do
+
+  end
+  test 'should save a valid review' do
+
+  end
+  test 'belongs to a user' do
+
+  end
+  test 'belongs to a product' do
+
+  end
 end
