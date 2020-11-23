f600727e10bd7d8645036b8c5a74cffde96930ed
Butler, Alexis (UG - Computer Science) <ab02259@surrey.ac.uk>
2020-11-23 16:28:19 +0000

    Section model tests

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,4 +2,6 @@
 #                                                       - Products are not allowed to exist if they don't have section
 class Section < ApplicationRecord
   has_many :products, dependent: :destroy
+  validates_presence_of :name
+  validates_uniqueness_of :name
 end
--- test/models/section_test.rb
+++ test/models/section_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class SectionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save without a name' do
+    sect = Section.new
+    assert !sect.save
+  end
+  test 'should save a valid section' do
+    sect = Section.new(name: "Unique sect name")
+    assert sect.save
+  end
+  test 'should not save a duplicate section' do
+    sect1 = Section.new(name: 'Unique sect name')
+    sect2 = Section.new(name: 'Unique sect name')
+    sect1.save
+    assert !sect2.save
+  end
+  test 'section should have products' do
+    assert_equal 2, sections(:one).products.size
+  end
 end
