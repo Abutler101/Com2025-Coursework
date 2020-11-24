c6e307b92aea795f86d262ef272ee100471fb37e
Butler, Alexis (UG - Computer Science) <ab02259@surrey.ac.uk>
2020-11-24 11:14:37 +0000

    Wrote order entry model tests

--- test/models/order_entry_test.rb
+++ test/models/order_entry_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class OrderEntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should have default quantity' do
+    ordent = OrderEntry.new(unitprice: 52, totalprice: 208, product: products(:one), order: orders(:one))
+    assert_equal(0, ordent.quantity)
+  end
+  test 'should have default unitprice' do
+    ordent = OrderEntry.new(quantity: 4, totalprice: 208, product: products(:one), order: orders(:one))
+    assert_equal(0, ordent.unitprice)
+  end
+  test 'should not save without product' do
+    ordent = OrderEntry.new(quantity: 4, unitprice: 52, totalprice: 208, order: orders(:one))
+    assert !ordent.save
+  end
+  test 'should not save without order' do
+    ordent = OrderEntry.new(quantity: 4, unitprice: 52, totalprice: 208, product: products(:one))
+    assert !ordent.save
+  end
+  test 'should save valid order entry' do
+    ordent = OrderEntry.new(quantity: 4, unitprice: 52, totalprice: 208, product: products(:one), order: orders(:one))
+    assert ordent.save!
+  end
+  test 'belongs to an order' do
+
+  end
+  test 'references a product' do
+
+  end
 end
