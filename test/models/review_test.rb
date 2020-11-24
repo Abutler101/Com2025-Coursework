c6a16111e2f9e91d64dd1d32f5f79a09391ec6a7
Butler, Alexis (UG - Computer Science) <ab02259@surrey.ac.uk>
2020-11-24 09:58:38 +0000

    Cleaned uploads folder

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -2,24 +2,29 @@ require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
   test 'should not save without product id' do
-
+    rev = Review.new(content: "Content", rating: 3, user: users(:one))
+    assert !rev.save
   end
   test 'should not save without user id' do
-
+    rev = Review.new(content: "Content", rating: 3, product: products(:one))
+    assert !rev.save
   end
   test 'should not save without content' do
-
+    rev = Review.new(content: "", rating: 3, product: products(:one), user: users(:one))
+    assert !rev.save
   end
   test 'should not save without rating' do
-
+    rev = Review.new(content: "Content", product: products(:one), user: users(:one))
+    assert !rev.save
   end
   test 'should save a valid review' do
-
+    rev = Review.new(content: "This is good", rating: 3, product: products(:one), user: users(:one))
+    assert rev.save
   end
   test 'belongs to a user' do
-
+    assert_equal(reviews(:one).user.firstName, users(:one).firstName)
   end
   test 'belongs to a product' do
-
+    assert_equal(reviews(:one).product.name, products(:one).name)
   end
 end
