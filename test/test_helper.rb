979b99e53dc1821fc2a6c306ca3406cd6289e493
Butler, Alexis (UG - Computer Science) <ab02259@surrey.ac.uk>
2020-11-26 19:47:16 +0000

    Realised seeding in test not working well. So removed

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,8 +5,6 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  # Add more helper methods to be used by all tests here...
-  Rails.application.load_seed
 
   def sign_in_as_tester
     post login_url, params: {email: users(:one).email, password: "t00l33t4u"}
