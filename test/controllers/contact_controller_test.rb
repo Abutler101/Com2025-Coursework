5fffa1e2bf435b09d258acc61ecdd4ba9149cb9a
Butler, Alexis (UG - Computer Science) <ab02259@surrey.ac.uk>
2020-11-27 09:17:05 +0000

    Finished contact controller tests

--- test/controllers/contact_controller_test.rb
+++ test/controllers/contact_controller_test.rb
@@ -1,8 +1,20 @@
 require 'test_helper'
-#TODO: write controller test for contact form
 class ContactControllerTest < ActionDispatch::IntegrationTest
-  test "should get contactus form" do
-
+  test 'should get contactus form' do
+    get getintouch_url
+    assert_response(:success)
+    assert_select 'form#ContactForm' do
+      assert_select 'input', 6 # text fields and the sub button
+    end
   end
-
+  test 'should accept a properly completed request' do
+    post submit_contact_url, params: {name: 'Alexis', email: 'lexieb101@gmail.com', telephone: '07809179117', message: '5ecurep@55w0rd'}
+    assert_redirected_to(root_url)
+    assert_equal "We'll be in touch", flash[:notice]
+  end
+  test 'should reject a malformed request' do
+    post submit_contact_url, params: {name: 'Alexis', email: '', telephone: '07809179117', message: '5ecurep@55w0rd'}
+    assert_redirected_to(root_url)
+    assert_equal 'Supply an email address for response', flash[:alert]
+  end
 end
