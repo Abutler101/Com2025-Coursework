6a084e07a913b561a7af00a28e79e28b400cf057
Butler, Alexis (UG - Computer Science) <ab02259@surrey.ac.uk>
2020-11-28 11:06:06 +0000

    Wrote and passed contact mailer tests + Removed todo from cart entry so that IDE stopped showing it in job list

--- test/mailers/contact_mailer_test.rb
+++ test/mailers/contact_mailer_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
-#TODO write Mailer tests
 class ContactMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should do a contact email' do
+    mail = ContactMailer.contact_email("ab02259@surrey.ac.uk", "Alexis Butler", "07809179117",
+                                       "Help I'm stuck writing rails tests")
+    assert_equal ['outreach@ab02259_coursework.com'], mail.to
+    assert_equal ['outreach@ab02259_coursework.com'], mail.from
+  end
 end
--- test/mailers/previews/contact_mailer_preview.rb
+++ test/mailers/previews/contact_mailer_preview.rb
@@ -1,7 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/contact_mailer
 class ContactMailerPreview < ActionMailer::Preview
   def contact_email
-    ContactMailer.contact_email("Alexis@me.com",
-                                "Alexis Butler", "07809179117", @message = "Hello")
+    ContactMailer.contact_email("Alexis@me.com", "Alexis Butler", "07809179117", @message = "Hello")
   end
 end
